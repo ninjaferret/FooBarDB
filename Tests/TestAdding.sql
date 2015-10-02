f0d0d14a820efd55f55901a42716708121f97d51
Ian Johnson <ian.johnson@red-gate.com>
2015-10-02 15:21:23 +0100

    Try fixing up the SQL

--- Tests/TestAdding.sql
+++ Tests/TestAdding.sql
@@ -5,11 +5,11 @@ CREATE PROCEDURE testAdd.[verify can add to list table]
 AS
 BEGIN
 	INSERT INTO [dbo].[ToDoList] ([Name])
-	VALUES(N'This is a name')
+	VALUES(N'This is a name');
 
 	DECLARE @count INT;
 
-	SELECT @count = COUNT(*) FROM [dbo].[ToDoList]
+	SELECT @count = COUNT(*) FROM [dbo].[ToDoList];
 
-	EXEC tSQLt.AssertEquals 1 @count
+	EXEC tSQLt.AssertEquals 1 @count;
 END
